85ecb80beafa41af5ed7ff271af6a4e01329ac0d
Rhoan Walker <rhoan.walker@gmail.com>
2018-07-04 20:46:15 -0500

    Modified C10_P04_5_Q02 file

--- Homework/C10_P04_5_Q02.java
+++ Homework/C10_P04_5_Q02.java
@@ -1,3 +1,6 @@
+// Class 10, Practice 4.5 - Question 2
+// String.compareTo function is used to determine the 
+
 import java.util.Scanner;
 
 class C10_P04_5_Q02
@@ -5,28 +8,28 @@ class C10_P04_5_Q02
 	public static void main(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
-		String nameOne;
-		String nameTwo;
+		String firstName;
+		String secondName;
 
 		System.out.print("Please enter the first name: ");
-		nameOne = scanner.next();
+		firstName = scanner.next();
 
-		System.out.print("Please enter the seecond name: ");
-		nameTwo = scanner.next();
+		System.out.print("Please enter the second name: ");
+		secondName = scanner.next();
 
-		int compareNames = nameOne.compareTo(nameTwo);
+		int compareNames = firstName.compareTo(secondName);
 
 		if (compareNames < 0 )
 		{
-			System.out.println("The name " + nameOne + " is first.");
+			System.out.println("The name " + firstName + " is first.");
 		}
 		else if (compareNames > 0)
 		{
-			System.out.println("The name " + nameTwo + " is first.");	
+			System.out.println("The name " + secondName + " is first.");	
 		}
 		else
 		{
-			System.out.println("Both names " + nameOne + " and " + nameTwo + " are the same.");
+			System.out.println("Both names " + firstName + " and " + secondName + " are the same.");
 		}
 
 	}
